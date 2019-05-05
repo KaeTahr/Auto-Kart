9c5e6c032ec8dfae7bf0f831d040bcc5f9036a82
zedehks <waldo.navascastellanos@gmail.com>
2019-05-05 17:15:47 -0600

    created a recycler view for the consulta

--- app/src/main/java/com/example/auto_kartprototype/consultarListas.java
+++ app/src/main/java/com/example/auto_kartprototype/consultarListas.java
@@ -2,12 +2,17 @@ package com.example.auto_kartprototype;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class consultarListas extends AppCompatActivity {
+
+    RecyclerView list = (RecyclerView) findViewById(R.id.grocery_list_list);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consultar_listas2);
+
+        
     }
 }
