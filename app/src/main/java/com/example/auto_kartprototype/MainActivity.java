958d7e5e26b349ae6dfc8c8eaa0b427c4da570ac
zedehks <waldo.navascastellanos@gmail.com>
2019-05-06 00:41:51 -0600

    Implemented 'swipe to delete' functionality to main list

--- app/src/main/java/com/example/auto_kartprototype/MainActivity.java
+++ app/src/main/java/com/example/auto_kartprototype/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.auto_kartprototype;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.VisibilityAwareImageButton;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DrawableUtils;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -106,9 +114,14 @@ public class MainActivity extends AppCompatActivity {
         lMan = new LinearLayoutManager(this);
         list.setLayoutManager(lMan);
 
-        adapter = new GroceryListAdapter(getApplicationContext(),List_GroceryLists);
+        adapter = new GroceryListAdapter(getApplicationContext(),List_GroceryLists,
+                this.findViewById(android.R.id.content));
         list.setAdapter(adapter);
+
+        ItemTouchHelper iTH = new ItemTouchHelper(new SwipeToDeleteThing((GroceryListAdapter) adapter));
+        iTH.attachToRecyclerView(list);
     }
+
 
 }
 
@@ -133,16 +146,24 @@ class GroceryListAdapter extends RecyclerView.Adapter<GroceryListAdapter.Grocery
         {
             Toast.makeText(v.getContext(),"you pressed a thing wow",Toast.LENGTH_SHORT).show();
         }
-
     }
 
     private ArrayList<GroceryList> list_of_shit;
     private Context context;
+    private GroceryList deletedItem;
+    private int deletedItemPos;
+    private View view;
 
-    public GroceryListAdapter(Context context, ArrayList<GroceryList> arrayList)
+    public GroceryListAdapter(Context context, ArrayList<GroceryList> arrayList, View view)
     {
         this.context = context;
         this.list_of_shit = arrayList;
+        this.view = view;
+    }
+
+    public Context getContext()
+    {
+        return this.context;
     }
 
     public void clearList()
@@ -178,6 +199,100 @@ class GroceryListAdapter extends RecyclerView.Adapter<GroceryListAdapter.Grocery
         super.onAttachedToRecyclerView(rV);
     }
 
+    public void deleteItem(int pos)
+    {
+        deletedItem = list_of_shit.get(pos);
+        deletedItemPos = pos;
+        list_of_shit.remove(pos);
+        notifyItemRemoved(pos);
+        showUndoSnackbar();
+    }
+    private void showUndoSnackbar()
+    {
+        Snackbar aloha = Snackbar.make(view,"Item Deleted.",Snackbar.LENGTH_LONG);
+        aloha.setAction("UNDO", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GroceryListAdapter.this.undoDelete();
+            }
+        });
+        aloha.show();
+    }
+    private void undoDelete()
+    {
+        list_of_shit.add(deletedItemPos,deletedItem);
+        notifyItemInserted(deletedItemPos);
+    }
 
 }
 
+class SwipeToDeleteThing extends ItemTouchHelper.SimpleCallback
+{
+    private GroceryListAdapter mAdapter;
+    private Drawable icon;
+    private final ColorDrawable background;
+
+    public SwipeToDeleteThing(GroceryListAdapter adapter)
+    {
+        super(0,ItemTouchHelper.LEFT/*|ItemTouchHelper.RIGHT*/);
+        mAdapter = adapter;
+        icon = ContextCompat.getDrawable(mAdapter.getContext(),R.drawable.ic_action_name);
+
+        background = new ColorDrawable(Color.RED);
+    }
+
+    @Override
+    public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+        // used for up and down movements
+        return false;
+    }
+
+    @Override
+    public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction)
+    {
+        int position = viewHolder.getAdapterPosition();
+        mAdapter.deleteItem(position);
+    }
+
+    @Override
+    public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder,
+                            float dX, float dY, int actionState,boolean isCurrentlyActive)
+    {
+        super.onChildDraw(c,recyclerView,viewHolder,dX,dY,actionState,isCurrentlyActive);
+        View itemView = viewHolder.itemView;
+        int backgroundCornerOffset = 20;
+
+        int iconMargin = (itemView.getHeight() - icon.getIntrinsicHeight()) / 2;
+        int iconTop = itemView.getTop() + (itemView.getHeight() - icon.getIntrinsicHeight()) / 2;
+        int iconBottom = iconTop + icon.getIntrinsicHeight();
+        //swipe right
+        if(dX > 0)
+        {
+            int iconLeft = itemView.getLeft() + iconMargin + icon.getIntrinsicWidth();
+            int iconRight = itemView.getLeft() + iconMargin;
+            icon.setBounds(iconLeft,iconTop,iconRight,iconBottom);
+
+            background.setBounds(itemView.getLeft(),itemView.getTop(),itemView.getLeft()
+                    + ((int) dX) + backgroundCornerOffset,itemView.getBottom());
+        }
+        //swipe left
+        else if (dX < 0)
+        {
+            int iconLeft = itemView.getRight() - iconMargin - icon.getIntrinsicWidth();
+            int iconRight = itemView.getRight() - iconMargin;
+            icon.setBounds(iconLeft,iconTop,iconRight,iconBottom);
+
+            background.setBounds(itemView.getRight() + ((int)dX)
+                    - backgroundCornerOffset,itemView.getTop(),
+                    itemView.getRight(),itemView.getBottom());
+        }
+        // no swipe
+        else
+        {
+            background.setBounds(0,0,0,0);
+        }
+        background.draw(c);
+        icon.draw(c);
+    }
+
+}
